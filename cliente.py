955879d0071f4f011ecde1624f82a64a6402befd
Nel Rondón <nelucho.nel@gmail.com>
2025-03-08 12:35:18 -0400

    modify the module cliente.py

--- cliente.py
+++ cliente.py
@@ -3,16 +3,22 @@ from handledb import DB
 
 class Feedback:
     def __init__(self, msg):
-        self.msg = msg
-        self.date = datetime.datetime.now()
+        self.message = msg
+        self.date = str(datetime.datetime.now())
+    
+    def export(self):
+        return self.__dict__
 
 class Cliente:
-    def __init__(self, data, name=None):
+    def __init__(self, data=None, name=None):
         self.id = data["id"] if data != None else str(uuid.uuid4())
         self.name = data["name"] if data != None else name
         self.frec_visit = data["frec_visit"] if data != None else 0
         self.feedback : list[dict] = data["feedback"] if data != None else []
-    
+
+    def addFeedback(self, msg):
+        self.feedback.append(Feedback(msg).export())
+
     def register(self):
         try:
             props = ["id", "name", "frec_visit", "feedback"]
